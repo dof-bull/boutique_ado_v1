6f149cd3499c7c5cada9bd3924f30843512ae3e0
dof-bull <donalofarrell1@gmail.com>
2020-08-18 09:35:24 +0000

    Added Stripe elements.

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HHQu4LMfxDapyn5gCJfdDe2r7gChHX0tZvH8oU5hFJrgPXQ1Zlq4pedoPIP2T9XmTfPJ8D0lZiz76rJ6uybR9gr00DXbVNjwq',
+        'client_secret': 'test client server',
     }
 
     return render(request, template, context)
